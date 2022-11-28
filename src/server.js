8951bec5e5fb05c4628348b42968bec643835c2f
Nguyenhoang291299 <hoang11761311@gmail.com>
2022-11-28 11:15:12 +0700

    main

--- src/server.js
+++ src/server.js
@@ -3,17 +3,34 @@ import initWebRoute from "./route/web";
 import initApiRoute from "./route/api";
 import connection from './configs/connectDB';
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
+var morgan = require('morgan');
+
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(morgan('combined'));
+
+app.use((req,res) => {
+  console.log("run into my middleware");
+  console.log(req.method);
+  console.log(req.header);
+})
 
 app.use(express.urlencoded({ extended:true}));
 app.use(express.json());
 configViewEngine(app);
+
 // init web route
 initWebRoute(app)
+
 // init api route
 initApiRoute(app)
+
+// handle 404 not found
+app.use((req,res) => {
+  return res.render('404.ejs')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
